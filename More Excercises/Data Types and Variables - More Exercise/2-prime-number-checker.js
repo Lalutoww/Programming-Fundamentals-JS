dc9a4b721c0a5ad7b71893752feefe66cd22f15c
Martin Lalutov <martinlalutov00@gmail.com>
2022-09-26 23:55:37 +0300

    Added more exercises
    Data Types and variables - more exercises -> done

---
+++ More Excercises/Data Types and Variables - More Exercise/2-prime-number-checker.js
@@ -0,0 +1,13 @@
+function primeNumber(number){
+    let isPrime = true;
+    for(let i = 2; i < number; i++){
+        if(number % i === 0){
+            isPrime = false;
+            break;
+        }
+    }
+    console.log(isPrime
+        ? 'true'
+        : 'false');
+}
+primeNumber(8)
---
+++ More Excercises/Data Types and Variables - More Exercise/4-biggest-of-three-numbers.js
@@ -0,0 +1,5 @@
+function biggestOfThree(num1,num2,num3){
+    let largestNum = Math.max(num1,num2,num3);
+    console.log(largestNum);
+}
+biggestOfThree(-2,7,3)
